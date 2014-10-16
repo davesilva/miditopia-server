046f3ec77f166c9e2171dee44483cd21a7fab8b2
David Silva <dsilva917@gmail.com>
2014-10-15 23:48:03 -0400

    Fix password validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 },
+                       confirmation: true,
+                       allow_blank: true
+  validates :password_confirmation, presence: true,
+                                    if: -> { password.present? }
   validates :email, presence: true,
                     uniqueness: true,
                     format: { with: /\A.*@.*\..*\z/ }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,25 @@ RSpec.describe User, type: :model do
   # Password
   it { should have_secure_password }
   it { should ensure_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of(:password) }
+
+  it 'should require a matching password and confirmation' do
+    u = FactoryGirl.build(:user, password: 'password123',
+                                 password_confirmation: 'password124')
+    expect(u).to_not be_valid
+  end
+
+  it 'should require a password_confirmation if password is present' do
+    u = FactoryGirl.build(:user, password_confirmation: nil)
+    expect(u).to_not be_valid
+  end
+
+  it 'should not require password if password_digest is present' do
+    FactoryGirl.create(:user)
+
+    u = User.first
+    expect(u).to be_valid
+  end
 
   # Email
   it { should validate_presence_of(:email) }

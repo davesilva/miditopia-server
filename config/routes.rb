c5fa2f63ac1048095644b70d395a2219fed79a7e
David Silva <dsilva917@gmail.com>
2014-10-18 10:30:37 -0400

    Create API v1 folder structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  api_version(:module => "V1", :path => {:value => "v1"}, :default => true) do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

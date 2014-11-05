7cdf800ccb28bf34e50c39ef2e35c829a57574da
David Silva <dsilva917@gmail.com>
2014-11-04 22:16:44 -0500

    Remove /api/ namespace on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  scope :api, defaults: { format: :json } do
-    api_version module: 'V1', path: { value: 'v1' } do
+  api_version module: 'V1', path: { value: 'v1' }, defaults: { format: :json } do
     resources :uploads
-      resources :users, only: [:create, :update, :show]
-      resources :tracks, only: [:index, :show, :create, :update, :destroy]
-      resources :uploads, only: [:index, :show, :create, :destroy]
-    end
+    resources :users, only: [:create, :update, :show]
+    resources :tracks, only: [:index, :show, :create, :update, :destroy]
+    resources :uploads, only: [:index, :show, :create, :destroy]
   end
 end

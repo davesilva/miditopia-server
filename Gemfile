5af2af92033164bea593792ee82823b93fc61430
David Silva <dsilva917@gmail.com>
2014-11-14 15:10:44 -0500

    Upgrade Ruby to 2.1.5

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.1.1'
+ruby '2.1.5'
 
 # Rails
 gem 'rails', '4.2.0.beta2'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET'],
-      region: ENV['S3_REGION']
+      region: ENV['S3_REGION'],
+      path_style: true
     }
 
     config.storage = :fog
